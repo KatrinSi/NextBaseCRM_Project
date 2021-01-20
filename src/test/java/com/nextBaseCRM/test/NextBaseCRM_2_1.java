248d6dc01548be6093c87e251388e9bccdc3392e
KatrinSi <katrinsi@me.com>
2021-01-20 13:49:06 -0500

    TC 2.1

--- src/test/java/com/nextBaseCRM/test/NextBaseCRM_2_1.java
+++ src/test/java/com/nextBaseCRM/test/NextBaseCRM_2_1.java
@@ -1,34 +1,73 @@
 package com.nextBaseCRM.test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.nextBaseCRM.test.Utilities.Sleep;
+import com.nextBaseCRM.test.Utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class NextBaseCRM_2_1 {
-    public static void main(String[] args) {
-
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        //1. User logs in to the NextBase CRM app with valid User Name and Password
+    WebDriver driver;
+    //1. User logs in to the NextBase CRM app with valid User Name and Password
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver("chrome");
         driver.get("https://login.nextbasecrm.com/");
+        Sleep.toSleep(1);
         driver.findElement(By.name("USER_LOGIN")).sendKeys("helpdesk21@cybertekschool.com");
         driver.findElement(By.name("USER_PASSWORD")).sendKeys("UserUser");
         driver.findElement(By.xpath("//input[@type='submit']")).click();
         driver.manage().window().maximize();
+    }
+    @Test
+    public void sendMessageTest() {
         //2. Click on "MESSAGE" tab
         driver.findElement(By.xpath("//span[@class='feed-add-post-form-link feed-add-post-form-link-active']//span[.='Message']")).click();
-        //Sleep.toSleep(100);
         //3. Type random text inside of the Message panel
-        driver.switchTo().frame(driver.findElement(By.tagName("iframe")));
-        driver.findElement(By.xpath("//iframe")).sendKeys("heloooooo");
-
+        Sleep.toSleep(3);
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='bx-editor-iframe']")));
+        driver.findElement(By.xpath("//body")).sendKeys("Hello Hello! How are you?");
         //4. Click on the "SEND" button
+        driver.switchTo().defaultContent();
+        driver.findElement(By.xpath("//button[@class='ui-btn ui-btn-lg ui-btn-primary']")).click();
+        Sleep.toSleep(5);
 
+        String actualResult = driver.findElement(By.xpath("//*[.='Hello Hello! How are you?'][1]")).getText();
+        Sleep.toSleep(1);
+        System.out.println(driver.findElement(By.xpath("//*[.='Hello Hello! How are you?'][1]")).getAttribute("id"));
+        String expectedResult = "Hello Hello! How are you?";
 
-
-        //toSleep(10);
-        //driver.close();
+        Assert.assertEquals(actualResult,expectedResult, "Test FAILED, actual result is not equal to expected.");
 
     }
+
+    @Test
+    public void cancelMessageTest(){
+
+        //2. Click on "MESSAGE" tab
+        driver.findElement(By.xpath("//span[@class='feed-add-post-form-link feed-add-post-form-link-active']//span[.='Message']")).click();
+        //3. Type random text inside of the Message panel
+        Sleep.toSleep(3);
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='bx-editor-iframe']")));
+        driver.findElement(By.xpath("//body")).sendKeys("Hello Hello! How are you?");
+        //4. Click on the "Cancel" button
+        driver.switchTo().defaultContent();
+        driver.findElement(By.id("blog-submit-button-cancel")).click();
+
+    }
+
+    @Test
+    @AfterMethod
+    public void teardownMethod() {
+        Sleep.toSleep(5);
+        driver.quit();
+    }
+
 }
+
+
+//how can I make sure that my message is sent? I don't have any confirm massage
+//what's the diff between Implicit wait and toSleep()
---
+++ src/test/java/com/nextBaseCRM/test/Utilities/WebOrderUtils.java
@@ -0,0 +1,21 @@
+package com.nextBaseCRM.test.Utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class WebOrderUtils {
+    public static void loginToSmartBear(WebDriver driver){
+        //3. Enter username: “Tester”
+        WebElement inputUsername = driver.findElement(By.id("ctl00_MainContent_username"));
+        inputUsername.sendKeys("Tester");
+
+        //4. Enter password: “test”
+        WebElement inputPassword = driver.findElement(By.id("ctl00_MainContent_password"));
+        inputPassword.sendKeys("test");
+
+        //5. Click to Login button
+        WebElement loginButton = driver.findElement(By.id("ctl00_MainContent_login_button"));
+        loginButton.click();
+    }
+}
